5e94f9c0653e2bd0a4fcd2c4e86955d6bb4080c8
Patryk Czarnecki <patryk@Patryks-Macbook.local>
2020-09-20 14:57:41 +0200

    Added more quiz questions

--- quiz_script.js
+++ quiz_script.js
@@ -6,6 +6,30 @@ var questions = [
     {
         prompt: "How many countries use Euro as their currency? \n a: 15 \n b: 19 \n c: 7",
         answer: "b"
+    },
+    {
+        prompt: "What is the capital of Australia? \n a: Sydney \n b: Canberra \n c: Melbourne",
+        answer: "b"
+    },
+    {
+        prompt: "How much is a dozen? \n a: 25 \n b: 20 \n c: 12",
+        answer: "c"
+    },
+    {
+        prompt: "In which year did Michael Jackson pass away? \n a: 2006 \n b: 2009 \n c: 2012",
+        answer: "b"
+    },
+    {
+        prompt: "How many planets are their in our solar system? (including Pluto). \n a: 9 \n b: 8 \n c: 7",
+        answer: "a"
+    },
+    {
+        prompt: "What is the name of the talking snowman in Disney's Frozen? \n a: Olaf \n b: Gandalf \b c: Harold",
+        answer: "a"
+    },
+    {
+        prompt: "In which year was the Microsoft XP operating system released? \n a: 1999 \n b: 2001 \n c: 2002",
+        answer: "b"
     }
 ]
 
